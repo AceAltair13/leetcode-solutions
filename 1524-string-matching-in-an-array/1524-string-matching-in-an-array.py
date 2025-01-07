7ce235e34155ef7705f90c61f065c73d7a73ccdc
Tirth Thoria <55307429+AceAltair13@users.noreply.github.com>
2025-01-06 23:38:36 -0800

    Time: 3 ms (64.01%), Space: 18 MB (6.61%) - LeetHub

--- 1524-string-matching-in-an-array/1524-string-matching-in-an-array.py
+++ 1524-string-matching-in-an-array/1524-string-matching-in-an-array.py
@@ -1,13 +1,10 @@
 class Solution:
     def stringMatching(self, words: List[str]) -> List[str]:
-        words.sort()
-        ans = set()
+        mega_string = ",".join(words)
+        ans = []
         
-        for i in range(len(words)):
-            for j in range(len(words)):
-                if i != j:
-                    pattern, word = words[i], words[j]
-                    if pattern in word:
-                        ans.add(pattern)
+        for word in words:
+            if mega_string.count(word) > 1:
+                ans.append(word)
         
-        return list(ans)
+        return ans
