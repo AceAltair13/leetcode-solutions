594880aeb26bcfb8185f835ec8772683795e7038
Tirth Thoria <55307429+AceAltair13@users.noreply.github.com>
2025-03-25 15:45:20 -0700

    Time: 0 ms (100%), Space: 14.8 MB (48.77%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> hashset;
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++) {
+            int complement = target - nums[i];
+            if (hashset.contains(complement)) {
+                return {hashset[complement], i};
+            }
+            hashset[nums[i]] = i;
+        }
+
+        return {};
+    }
+};
