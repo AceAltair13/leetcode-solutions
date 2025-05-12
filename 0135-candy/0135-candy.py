2f9837180af7057b05e662845639f742160ff24c
Tirth Thoria <55307429+AceAltair13@users.noreply.github.com>
2025-05-11 23:13:43 -0700

    Time: 19 ms (40.97%), Space: 19.8 MB (69.53%) - LeetHub

--- 0135-candy/0135-candy.py
+++ 0135-candy/0135-candy.py
@@ -5,11 +5,13 @@ class Solution:
 
         n = len(ratings)
         candies = [1] * n
-
+        
+        # Left to right
         for i in range(1, n):
             if ratings[i] > ratings[i - 1]:
                 candies[i] = candies[i - 1] + 1
 
+        # Right to left
         for j in range(n - 2, -1, -1):
             if ratings[j] > ratings[j + 1]:
                 candies[j] = max(candies[j + 1] + 1, candies[j])
